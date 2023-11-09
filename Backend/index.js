2a9fe93cc67de0b5f8bb4080cc4dbc8961a8a9f2
Karthik Vijay <kv52274@gmail.com>
2023-11-09 19:32:48 +0530

    Add MongoDB connection and user authentication endpoints

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,9 @@
+//Using MONGO_CONNECT from .env file due to some network and connection issues. DB_CONNECT can also be used.
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
 const port = 3001;
 const cors = require('cors');
 
@@ -7,19 +11,49 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.static('public'));
+dotenv.config();
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Hello World' });
+mongoose.connect(process.env.MONGO_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => {
+    console.log('Connected to MongoDB Database');
 });
 
-app.post('/signup', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Hello World SignUp' });
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
 });
 
-app.get('/login', (req, res) => {
-    res.send('Hello World!');
+const UserModel = mongoose.model('auth', userSchema);
+
+
+app.post('/login', async (req, res) => {
+    // console.log(req.body);
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username, password });
+    if (!user) {
+        res.status(401).json({ message: 'Invalid Credentials,Please SignUp first' });
+    } else {
+        res.json({ message: 'Login Success' });
+    }
+});
+
+app.post('/signup', async (req, res) => {
+    // console.log(req.body);
+    try {
+        const userData = req.body;
+        const newUser = new UserModel(userData);
+        await newUser.save();
+        res.json('User added to MongoDB!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error adding user to MongoDB');
+    }
 });
 
 app.listen(port, () => {
