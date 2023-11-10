3128a98828ed3327878a2ab540365afcb361d626
Karthik Vijay <kv52274@gmail.com>
2023-11-10 21:56:58 +0530

    Added navbar to homepage

--- Frontend/app/Home/page.js
+++ Frontend/app/Home/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+'use client'
+import '../styles/global.css';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const handleLogout = async () => {
+  window.location.replace('Login');
 }
+const Page = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+      <h2 style={{ color: 'black', margin: '2px' }}>Event <span style={{color:"red"}}>Lister</span></h2>
+      <button style={{ backgroundColor: 'rgb(249, 49, 49)', color: 'white', padding: '7px 25px', textAlign: 'center', textDecoration: 'none', display: 'inline-block', fontSize: '16px', marginRight: '2px', cursor: 'pointer',borderRadius:"20px" }} onClick={handleLogout}>
+        Log Out
+      </button>
+    </div>
+  );
+};
 
-export default page
+export default Page;
